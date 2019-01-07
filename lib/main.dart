1a8176f0edf164d0c1b1014acc5624c77f5cde15
chatethakhun <chatethakhun@hubbathailand.com>
2019-01-07 15:13:17 +0700

    create mycard stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,102 +23,56 @@ class HomePage extends StatelessWidget {
         ),
         body: ListView(
           children: <Widget>[
-            Card(
-                margin: EdgeInsets.all(30),
-                elevation: 10,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 200,
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(0),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  'https://www.gstatic.com/webp/gallery/5.jpg'))),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                          'Fear lead to anger...anger leads to hate..hate'),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 10, 10),
-                      alignment: Alignment(1, 0),
-                      child: Text(
-                        'Yoda (Phantom Manance)',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Card(
-                margin: EdgeInsets.all(30),
-                elevation: 10,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 200,
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(0),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  'https://www.gstatic.com/webp/gallery/5.jpg'))),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                          'Fear lead to anger...anger leads to hate..hate'),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 10, 10),
-                      alignment: Alignment(1, 0),
-                      child: Text(
-                        'Yoda (Phantom Manance)',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Card(
-                margin: EdgeInsets.all(30),
-                elevation: 10,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 200,
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(0),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  'https://www.gstatic.com/webp/gallery/5.jpg'))),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                          'Fear lead to anger...anger leads to hate..hate'),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 10, 10),
-                      alignment: Alignment(1, 0),
-                      child: Text(
-                        'Yoda (Phantom Manance)',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                      ),
-                    )
-                  ],
-                ))
+            MyCard('111', 'Yoda', 'https://www.gstatic.com/webp/gallery/4.jpg'),
+            MyCard('111', 'Sky walker',
+                'https://www.gstatic.com/webp/gallery/1.jpg'),
+            MyCard('111', '444', 'https://www.gstatic.com/webp/gallery/2.jpg'),
+            MyCard('111', '444', 'https://www.gstatic.com/webp/gallery/3.jpg'),
+          ],
+        ));
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final String _text;
+  final String _author;
+  final String _imageUrl;
+  const MyCard(
+    this._text,
+    this._author,
+    this._imageUrl, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: EdgeInsets.all(30),
+        elevation: 10,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(0),
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage('$_imageUrl'))),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('$_text'),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 10, 10),
+              alignment: Alignment(1, 0),
+              child: Text(
+                '$_author',
+                style: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            )
           ],
         ));
   }
